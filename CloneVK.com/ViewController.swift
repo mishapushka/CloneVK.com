7ab8af5d0be47424ef1706fb35383994666fcb0a
mishapushka <bskfrost3@yandex.ru>
2022-01-02 16:38:12 +0700

    Initial Commit

---
+++ CloneVK.com/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloneVK.com
+//
+//  Created by Mikhail Pushkarev on 02.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
