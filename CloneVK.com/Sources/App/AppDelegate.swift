f72ceb52b548391aac176be4b39f0cd378811541
mishapushka <bskfrost3@yandex.ru>
2022-01-02 17:08:39 +0700

    Delete SceneDelegate. Add navigation group.

---
+++ CloneVK.com/Sources/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CloneVK.com
+//
+//  Created by Mikhail Pushkarev on 02.01.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
